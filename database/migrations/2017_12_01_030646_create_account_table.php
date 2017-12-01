c76d5e2575291ac30ded0846d0e4f7b26e17b271
HyperQing <469379004@qq.com>
2017-12-01 16:10:57 +0800

    完成用户表创建

--- database/migrations/2017_12_01_030646_create_account_table.php
+++ database/migrations/2017_12_01_030646_create_account_table.php
@@ -20,8 +20,8 @@ class CreateAccountTable extends Migration
             $table->engine = 'InnoDB';
             $table->bigIncrements('user_id')->comment('用户id');
             $table->string('email')->comment('用户邮箱');
-            $table->string('username')->comment('用户名');
-            $table->char('password', 60)->comment('密码密文');
+            $table->string('user_name')->comment('用户名');
+            $table->char('user_password', 60)->comment('密码密文');
             $table->rememberToken(); // 加入 remember_token 并使用 VARCHAR(100) NULL。
             $table->timestamps(); // 加入 created_at 和 updated_at 字段。
         });
