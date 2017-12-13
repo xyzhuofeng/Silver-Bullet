ae1b3ccdae88b776e22c630b69f35b9b86c9a26a
HyperQing <469379004@qq.com>
2017-12-13 15:33:22 +0800

    更新项目文件表迁移类

--- database/migrations/2017_12_13_150732_create_project_file.php
+++ database/migrations/2017_12_13_150732_create_project_file.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
+/**
+ * 创建项目文件表
+ */
 class CreateProjectFile extends Migration
 {
     /**
@@ -13,8 +16,13 @@ class CreateProjectFile extends Migration
      */
     public function up()
     {
-        Schema::table('=blade_project_file', function (Blueprint $table) {
-            //
+        Schema::create('blade_project_file', function (Blueprint $table) {
+            $table->bigIncrements('file_id')->comment('文件id');
+            $table->bigInteger('creator')->comment('文件创建者id');
+            $table->bigInteger('project_id')->comment('项目id');
+            $table->text('virtual_path')->comment('文件虚拟路径，如“全部文件/项目文件”');
+            $table->string('real_name', 255)->comment('真实文件名');
+            $table->timestamps();
         });
     }
 
@@ -25,8 +33,6 @@ class CreateProjectFile extends Migration
      */
     public function down()
     {
-        Schema::table('=blade_project_file', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('blade_project_file');
     }
 }
