161ae74b916c714ec51fc5a5feb6e67378e081d8
HyperQing <469379004@qq.com>
2018-04-05 15:23:13 +0800

    WIKI添加目录树功能

--- resources/assets/js/components/WikiItem.vue
+++ resources/assets/js/components/WikiItem.vue
@@ -1,16 +1,26 @@
 <template>
     <div class="container">
-        <article>
-            <div class="title">
-                <el-input placeholder="请在此输入标题" v-model="current.title"></el-input>
-            </div>
-            <div class="content">
-                <el-input type="textarea" placeholder="请在此输入内容" :rows="25" v-model="current.content"></el-input>
-            </div>
-            <div class="btn-row">
-                <el-button type="primary" @click="save">保存</el-button>
-            </div>
-        </article>
+        <el-row>
+            <el-col :span="6">
+                <h3>目录</h3>
+                <el-tree :data="data" node-key="id" default-expand-all></el-tree>
+            </el-col>
+            <el-col :span="18">
+                <article>
+                    <div class="title">
+                        <el-input placeholder="请在此输入标题" v-model="current.title"></el-input>
+                    </div>
+                    <div class="content">
+                        <el-input type="textarea" placeholder="请在此输入内容" :rows="25" v-model="current.content"></el-input>
+                    </div>
+                    <div class="btn-row">
+                        <el-button type="primary" @click="save">保存</el-button>
+                        <el-button>添加</el-button>
+                        <el-button>删除</el-button>
+                    </div>
+                </article>
+            </el-col>
+        </el-row>
     </div>
 </template>
 
@@ -22,7 +32,16 @@
                 current: {
                     title: "",
                     content: "",
-                }
+                },
+                data: [{
+                    label: '开发设计文档',
+                }, {
+                    label: '账号模块',
+                }, {
+                    label: '成员管理模块',
+                }, {
+                    label: 'API文档',
+                }],
             }
         },
         methods: {
@@ -64,7 +83,7 @@
 <style scoped>
     .container {
         width: 100%;
-        max-width: 800px;
+        max-width: 1000px;
         margin: 0 auto;
     }
 
